605a02dfda00ef8b2803cfa4a9c129a2ea9680f8
OsmanBytyqi <obytyqi2001@gmail.com>
2021-08-08 00:09:04 +0200

    array and object

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,8 +1,26 @@
-// const inputs=document.querySelectorAll('input');
-// inputs.forEach(input=>{
-//     console.log(input);
-// }
-var circle = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['mani', 'mario', 'gent'];
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+var numbers = [10, 20, 12, 15];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('red');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var man = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circle(4));
+man.age = 40;
+man.name = 'ryu';
+man = {
+    name: 'mani',
+    belt: 'orange',
+    age: 40
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,36 @@
+// arrays
+let names = ['mani', 'mario', 'gent'];
+
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('red');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let man = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+man.age = 40;
+man.name = 'ryu';
 
 
-// const inputs=document.querySelectorAll('input');
-
-// inputs.forEach(input=>{
-//     console.log(input);
-// }
-
-const circle=(diameter:number)=>{
-    return diameter*Math.PI;
-}
-
-console.log(circle(4));
+man = {
+  name: 'mani',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
