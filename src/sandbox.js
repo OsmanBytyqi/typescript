fa390c8616592c06b77c9d8692f8386adf88fde6
OsmanBytyqi <obytyqi2001@gmail.com>
2021-08-09 17:41:01 +0200

    types of data

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,24 +1,17 @@
-var character = 'osi';
-var age;
-var isLoggedIn;
-// age = 'luigi';
-age = 30;
-// isLoggedIn = 25;
-isLoggedIn = true;
-// arrays
-var manis = [];
-manis.push('gen');
-manis.push('gu');
-console.log(manis);
-// union types
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'wow' };
+console.log(age);
 var mixed = [];
-mixed.push('hello');
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-mixed.push(20);
 console.log(mixed);
-var uid;
-// objects
 var mani;
-mani = { name: 'yoshi', age: 30 };
-var osi;
-osi = { name: 'ken', age: 20, beltColour: 'black' };
+mani = { name: 'wow', age: 25 };
+console.log(mani);
+mani = { name: 25, age: 'wow' };
+console.log(mani);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,37 @@
 let age: any = 25;
 
 age = true;
+
 console.log(age);
+
 age = 'hello';
+
+
 console.log(age);
+
 age = { name: 'wow' };
+
 console.log(age);
 
 let mixed: any[] = [];
 
 mixed.push(5);
+
 mixed.push('mario');
+
 mixed.push(false);
+
 console.log(mixed);
 
 let mani: { name: any, age: any };
 
 mani = { name: 'wow', age: 25 };
+
+
 console.log(mani);
 
 mani = { name: 25, age: 'wow' };
+
+
+
 console.log(mani);
