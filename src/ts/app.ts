e1db0be8d9dd9c8babc877d299c53784c6aafa84
OsmanBytyqi <obytyqi2001@gmail.com>
2021-08-15 00:46:14 +0200

    identifiers of class

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,29 +1,31 @@
 // classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string, 
+    private details: string, 
+    public amount: number,
+  ){}
 
   format() {
     return `${this.client} owes £${this.amount} for ${this.details}`;
   }
 }
 
-const invOne = new Invoice('mani', 'work on the genti website', 250);
-const invTwo = new Invoice('genti', 'work on the mani website', 300);
-// declared an array which is empty and can accept just object 
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne)
 invoices.push(invTwo);
 
-console.log(invoices);
+invoices.forEach(inv => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
+
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
